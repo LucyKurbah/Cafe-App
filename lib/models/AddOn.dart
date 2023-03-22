40783f371dbeec6bb1b60a533205b30a4002ac5b
LucyKurbah <53791871+LucyKurbah@users.noreply.github.com>
2023-03-22 18:00:26 +0600

    Add files via upload

---
+++ lib/models/AddOn.dart
@@ -0,0 +1,30 @@
+class AddOn{
+    
+    int? id;
+    late String title, image, desc;
+    late double price;
+
+    AddOn({required this.title, required this.image, required this.price, required this.id, required this.desc});
+
+  factory AddOn.fromJson(Map<String, dynamic> json){
+  
+    return AddOn(
+      id : json['id'],
+      title : json['food_name'],
+      image : json['path_file'],
+      price : double.parse(json['price']),
+      desc : json['description'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'desc' : desc,
+      'price' : price,
+      'image' :image
+    };
+  }
+
+}
