810fea5a7de5f3b2910fbe0eb3cba7a377f6c5af
LucyKurbah <53791871+LucyKurbah@users.noreply.github.com>
2023-03-22 18:03:25 +0600

    Add files via upload

--- lib/api/apiFile.dart
+++ lib/api/apiFile.dart
@@ -1,13 +1,19 @@
 class ApiConstants {
-  //ip= "10.179.2.187"
-  static String baseUrl = 'http://13.231.134.208/api';
-  static String ipUrl = 'http://13.231.134.208';
+  static String aws_ip= "13.231.134.208";
+  static String office_ip="10.179.2.187:8000";
+  static String home_ip="";
+
+  static String right_now_ip=office_ip;
+
+  static String baseUrl = 'http://'+right_now_ip+'/api';
+  static String ipUrl = 'http://'+right_now_ip;
   static String loginUrl = '$baseUrl/login';
   static String registerUrl = '$baseUrl/register';
   static String forgotPasswordUrl = '$baseUrl/forgotPassword';
   static String logoutUrl = '$baseUrl/logout';
   static String userUrl = '$baseUrl/user';
   static String itemUrl = '$baseUrl/getFoodItems';
+  static String getAddOnUrl = '$baseUrl/getAddOnItems';
   static String tableUrl = '$baseUrl/getTables';
   static String getTableDetailsUrl = '$baseUrl/getTableDetails';
   static String getConferenceDetailsUrl = '$baseUrl/getConferenceDetails';
@@ -15,7 +21,7 @@ class ApiConstants {
   static String addCartUrl = '$baseUrl/cart/add';
   static String removeCartUrl = '$baseUrl/cart/remove';
   static String getTotalUrl = '$baseUrl/cart/getTotal';
-  static String imagePath = "http://13.231.134.208/";
+  static String imagePath = "http://"+right_now_ip+"/";
   static String imageUrl = "$baseUrl/$imagePath";
   static String serverError = 'Servor Error';
   static String unauthorized = 'Unauthorized';
